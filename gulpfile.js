037a7dbc93bd3ef07538ad74849639b63cf4c5a4
kang <kangjm103@naver.com>
2019-08-22 18:51:48 +0900

    # task 명칭 명시적으로 변경

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('copy', () => {
 });
 
 // combine:js 테스크를 정의
-gulp.task('main', () => {
+gulp.task('js-ugly', () => {
     // src 하위 디렉터리 내의 모든 자바스크립트 파일을 가져온다.
     // !는 바로 앞의 와일드카드 형태에서 이 파일을 포함하고 있을 경우에만 이것을 사용 가능
     return gulp
@@ -55,4 +55,4 @@ gulp.task('main', () => {
 
 // Gulp.task() 를 사용해 기본(Default) 테스크를 정의
 // Gulp.task 의 파라미터: ( 수행할 업무 이름, [task 를 수행하기 전에 먼저 실행되어야하는 task 들의 배열 목록>> 생략 가능], 처리해야할 업무 프로세스 )
-gulp.task('default', ['clear', 'copy', 'main']);
+gulp.task('default', ['clear', 'copy', 'js-ugly']);
