31b7d9455301fa9a33f564cfc53209c7d1f44714
kang <kangjm103@ex-em.com>
2023-05-15 11:29:06 +0900

    Update gulp project ##### - gulp version up - copy, add 관련 수정 및 경로 간략화 - update babel - src 폴더 내의 프로젝트별 세팅과 동적 스크립트 실행 가능하도록 수정 - etc 폴더 적용 (특정 파일들만 추출하여 진행하고 싶을 때 활용)

--- README.md
+++ README.md
@@ -7,16 +7,40 @@
 > 4. result 폴더 생성
 
 ## 실행
+
+두 가지 방법이 있음
+
+#### 1. src 폴더 > 통째로 진행
 > 1. src 폴더에 build 될 소스 넣기
->    - ex) mklink /d 원본소스경로\MAXGAUGE_MYUNG gulp프로젝트경로\MAXGAUGE
-> 2. npm run gulp
+>> ex)
+>> - mklink /d gulp프로젝트경로\src\MAXGAUGE 원본소스경로\MAXGAUGE
+> 2. script 실행
+>> ex)
+>> - npm run gulp
 > 3. result 폴더에서 확인
+
+#### 2. src 폴더 > 특정 폴더 지정
+> 1. src 폴더에 build 될 소스 넣기
+>> ex)
+>> - mklink /d gulp프로젝트경로\src\mfo\MAXGAUGE 원본소스경로\MAXGAUGE
+>> - mklink /d gulp프로젝트경로\src\mft\MAXGAUGE 원본소스경로\MAXGAUGE
+>> - mklink /d gulp프로젝트경로\src\mfa\MAXGAUGE 원본소스경로\MAXGAUGE
+>> - mklink /d gulp프로젝트경로\src\\???\MAXGAUGE 원본소스경로\MAXGAUGE
+> 2. script 실행
+>> ex)
+>> - npm run gulp mfo
+>> - npm run gulp mft
+>> - npm run gulp mfa
+>> - npm run gulp ???
+> 3. result 폴더에서 확인
+
+ps . 가끔 result 폴더 클린시 에러가 나는 경우 발생  한번 더 실행하면 정상 동작
 
 ## task list
 > - clear (result 폴더 모든 내용 제거)
 > - copyAllFilesExcludeJS (.js 파일을 제외한 파일들 move)
 > - copyNotUglifyJS (uglify 할 필요 없는 js 들 move)
-> - uglifyJS (.js 파일들 uglify) 
+> - uglifyJS (.js 파일들 uglify)
 
 ## 처음부터 직접 의존성 추가 해줄 경우
 > 1. npm init
--- gulpfile.js
+++ gulpfile.js
@@ -2,94 +2,110 @@
  * Created by Kang on 2018-04-23.
  */
 
-const { series, parallel, src, dest, lastRun } = require('gulp');
+const {series, parallel, src, dest, lastRun, task} = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const clean = require('gulp-rimraf');
-const replace = require("gulp-replace");
+const replace = require('gulp-replace');
 const stripDebug = require('gulp-strip-debug');   // 모든 console.log 들과 alert 들을 제거해주는 gulp 모듈 패키지
+const folderName = process.title.split(' ')[1];
+let taskName;
+let root;
+
+if (folderName) {
+  taskName = folderName;
+  root = `src/${folderName}`;
+} else {
+  taskName = 'default';
+  root = 'src';
+}
 
 // 기존 dist 폴더 clear
 function clear() {
-    return src('result/*', {
-            read: false
-        })
-        .pipe(clean());
+  return src('result/*', {
+    read: false,
+  }).pipe(clean());
 }
 
 // copy all files
 function copyAllFilesExcludeJS() {
-    return src(
-            [
-                'src/**/**/*.*',
-                '!src/**/**/*.js',
-                '!src/**/**/.idea/**/**/*.*',
-                '!src/**/**/.git/**/**/*.*',
-                '!src/**/**/.gitignore'
-            ],
-            {
-                since: lastRun(copyAllFilesExcludeJS)
-            }
-        )
-        .pipe(dest('result'))
+  return src(
+    [
+      `${root}/**/**/*.*`,
+      `!${root}/**/**/*.{js,yml}`,
+      `!${root}/**/**/.{git,idea}/**/**/*.*`,
+      `!${root}/**/**/.gitignore`,
+    ],
+    {
+      since: lastRun(copyAllFilesExcludeJS),
+    },
+  ).pipe(dest('result'));
 }
 
 function copyNotUglifyJS() {
-    return src(
-            [
-                'src/**/**/extjs/**/**/*.js',
-                'src/**/**/lib/**/**/*.js',
-                'src/**/**/liteplus/**/**/*.js'
-            ],
-            {
-                since: lastRun(copyNotUglifyJS)
-            }
-        )
-        .pipe(dest('result'))
+  return src(
+    [
+      `${root}/**/**/extjs/**/**/*.js`,
+      `${root}/**/**/lib/**/**/*.js`,
+      `${root}/**/**/liteplus/**/**/*.js`,
+    ],
+    {
+      since: lastRun(copyNotUglifyJS),
+    },
+  ).pipe(dest('result'));
 }
 
 // combine:js 테스크를 정의
 function uglifyJS() {
-    // src 하위 디렉터리 내의 모든 자바스크립트 파일을 가져온다.
-    // !는 바로 앞의 와일드카드 형태에서 이 파일을 포함하고 있을 경우에만 이것을 사용 가능
-    return src(
-            [
-                'src/**/app.js',
-                'src/**/**/common/**/**/*.js',
-                'src/**/**/config/**/**/*.js',
-                'src/**/**/Exem/**/**/*.js',
-                'src/**/**/PA/**/**/*.js',
-                'src/**/**/popup/**/**/*.js',
-                'src/**/**/RTM/**/**/*.js',
-                'src/**/**/lib/IMXWS.js'
-            ],
-            {
-                sourcemaps: true
-            },
-            {
-                since: lastRun(uglifyJS)
-            }
-        )
-        .pipe(babel({
-            presets: ['@babel/preset-env']
-        }))
-        .pipe(stripDebug())
-        // .pipe(uglify({
-        //     mangle: {
-        //         toplevel: true,
-        //         reserved: ['jQuery']
-        //     }
-        // }))
-        .pipe(uglify())
-        .pipe(replace(/('|")use strict\1;/g, ''))
-        .pipe(dest('result'));
+  // src 하위 디렉터리 내의 모든 자바스크립트 파일을 가져온다.
+  // !는 바로 앞의 와일드카드 형태에서 이 파일을 포함하고 있을 경우에만 이것을 사용 가능
+  return src(
+    folderName === 'etc'
+      ? `${root}/**/*.js`
+      : [
+        `${root}/**/{app,appHyundaiCard}.js`,
+        `${root}/**/**/{common,config,Exem,RTM,PA,popup}/**/**/*.js`,
+        `${root}/**/**/lib/IMXWS.js`,
+      ],
+    {
+      sourcemaps: true,
+    },
+    {
+      since: lastRun(uglifyJS),
+    }
+  )
+    .pipe(babel())
+    .pipe(stripDebug())
+    // .pipe(uglify({
+    //     mangle: {
+    //         toplevel: true,
+    //         reserved: ['jQuery']
+    //     }
+    // }))
+    .pipe(uglify())
+    .pipe(replace(/('|")use strict\1;/g, ''))
+    .pipe(dest('result'));
 }
 
-exports.default = series(
-    clear,
+
+/**
+ * 1. npm run gulp
+ *  - 뒤에 아무것도 없으면 default 로 default (src 폴더 전체)가 세팅됨
+ * 2. npm run gulp [특정 폴더명]
+ *  - 특정 폴더명을 입력 시, 해당 폴더만 진행 (단, maxgauge 프로젝트 압축 범위 적용해서 진행)
+ *  - 특정 폴버명이 etc 일 경우, etc 폴더 안에 있는 모든 파일들을 대상으로 진행
+ */
+task(
+  taskName,
+  series(clear,
+    (done) => {
+      console.log('src 빌드중');
+      done();
+    },
     parallel(
-        copyAllFilesExcludeJS,
-        copyNotUglifyJS,
-        uglifyJS
-    ),
+      copyAllFilesExcludeJS,
+      copyNotUglifyJS,
+      uglifyJS
+    )
+  )
 );
